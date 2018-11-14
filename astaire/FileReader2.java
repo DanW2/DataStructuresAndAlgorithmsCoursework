cbe09def2aad8c1389937902fa78b4248cd02afd
NBatchelor1 <44847054+NBatchelor1@users.noreply.github.com>
2018-11-14 16:51:21 +0000

    Changed the class
    Changed the class so it should read the csv files however cannot get it to locate the files

--- astaire/FileReader2.java
+++ astaire/FileReader2.java
@@ -2,32 +2,36 @@ package astaire;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class FileReader2 {
 	public static void main(String[] args) {
+		String csvFile = ".../astaire.assets/danceShowData_dances.csv";
+		BufferedReader br = null;
+		String line = "";
+		String cvsSplitBy = ",";
 		
-	
-	BufferedReader reader = null;
-
-	try {
-	    File file = new File("sample-file.dat");
-	    reader = new BufferedReader(new FileReader(file));
-
-	    String line;
-	    while ((line = reader.readLine()) != null) {
-	        System.out.println(line);
-	    }
-
-	} catch (IOException e) {
-	    e.printStackTrace();
-	} finally {
-	    try {
-	        reader.close();
-	    } catch (IOException e) {
-	        e.printStackTrace();
-	    }
-	}
+		try {
+			br = new BufferedReader(new FileReader(csvFile));
+			while ((line = br.readLine()) != null) {
+				//using commas as seperators
+				String[] dances = line.split(cvsSplitBy);
+				System.out.println(dances);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 }	
 }
